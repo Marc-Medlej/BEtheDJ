a7ffe008ccd915acb8a03cda60e1138ad87c6dbe
Simon Walton <saw29@hotmail.co.uk>
2020-08-24 18:55:09 +0100

    new map added to venue index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_142357) do
+ActiveRecord::Schema.define(version: 2020_08_24_155518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2020_08_24_142357) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   add_foreign_key "event_tracks", "events"
