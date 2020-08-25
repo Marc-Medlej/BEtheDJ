812e66e7a51147131b0754a2a757ec02e9a1b8b6
Elena Dinca <elenadinca25@gmail.com>
2020-08-25 01:16:55 +0100

    added  seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Venue.destroy_all
+User.destroy_all
+Track.destroy_all
+
+venues =  Venue.create(
+    name: "Venues + Events Live",
+    location: "Harbour Exchange"
+  )
+
+
+venues = Venue.create(
+    name: "LightHouse",
+    location: "London"
+  )
+
+
+users = User.create(
+  first_name: "William",
+  last_name: "Brown",
+  email: "WilliamBrown@gmail.com",
+  dj_name: "Will",
+  password: "123456"
+  )
+
+
+
+users = User.create(
+  first_name: "Bill",
+  last_name: "White",
+  email: "BillWhite@gmail.com",
+  dj_name: "Billy",
+  password: "123456"
+  )
+
+
+tracks = Track.create(
+  name: "Pirate Baird O'Connell",
+  artist: "Billie Eilish",
+  genre: "streambait"
+  )
+
+tracks = Track.create(
+  name: "Pirate Baird O'Connell",
+  artist: "Billie Eilish",
+  genre: "streambait"
+  )
+
+events = Event.create(
+  date: "12.09.2020",
+  venue_id: Event.first,
+  user_id: Event.first,
+  name: "Untold"
+  )
+
+events = Event.create(
+  date: "08.09.2020",
+  venue_id: Venue.first,
+  user_id: User.first,
+  name: "NeverSea"
+  )
+
+event_tracks = EventTrack.create(
+  already_played: false,
+  track_id: 1,
+  event_id: 2
+  )
+
+event_tracks = EventTrack.create(
+  already_played: true,
+  track_id: 3,
+  event_id: 4
+  )
+
