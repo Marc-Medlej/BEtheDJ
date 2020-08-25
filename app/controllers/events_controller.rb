8b6afdd7bb8d17086e5302e8a3848bc7518f34c2
Marc Medlej <medlejmarc@gmail.com>
2020-08-25 11:36:04 +0200

    Show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
     def show
         @event = Event.find(params[:id])
         @tracks = @event.tracks
-        @track_request_count = Track.joins(:requests).group('tracks.id').count
+        @track_request_count = Track.joins(:requests).group('tracks.id').count 
     end
 
     def new
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,9 +4,12 @@ class RequestsController < ApplicationController
 
     def create
         @request = Request.new
-        @request.event = Event.find(params[:event_id])
+        @event = Event.find(params[:event_id])
+        @request.event = @event
         @request.track = Track.find(params[:track_id])
         @request.user = current_user
         @request.save
+
+        redirect_to event_path(@event)
     end
 end
