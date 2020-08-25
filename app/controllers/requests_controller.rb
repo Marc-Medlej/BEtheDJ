e29f314fcb89a4c937afe85a924584ef32bab3f3
Marc Medlej <medlejmarc@gmail.com>
2020-08-25 13:48:27 +0200

    Validation, still working on it

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,7 @@
 class RequestsController < ApplicationController
     def index
+        @event = Event.find(params[:id])
+        @requested_tracks = Track.joins(:requests).where(requests: { event_id: @event.id }).where(requests: { user_id: current_user.id })    
     end
 
     def create
@@ -8,8 +10,15 @@ class RequestsController < ApplicationController
         @request.event = @event
         @request.track = Track.find(params[:track_id])
         @request.user = current_user
-        @request.save
+        @tracks = @event.tracks
+        @track_request_count = Track.joins(:requests).group('tracks.id').count
 
-        redirect_to event_path(@event)
+        if @request.save
+            redirect_to event_path(@event)
+        else
+            flash[:notice] = "You have already requested the song"
+            render 'events/show' 
+        end
+
     end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,6 @@ class Request < ApplicationRecord
   belongs_to :event
   belongs_to :track
   belongs_to :user
+
+  validates_uniqueness_of :user_id, scope: [:track_id, :event_id], message: 'You have already requested that song' 
 end
